a5338e5bd2791c5d59d90bca836d5a15d54873f1
Gaurav Sagar <gaurav.sagar@drcsystems.com>
2021-11-10 16:59:11 +0530

    -Table One Completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TableAccordionComponent } from './table-accordion/table-accordion.component';
+import { TableOneComponent } from './table-one/table-one.component';
+import { TableTwoComponent } from './table-two/table-two.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'table-one',
+    component:TableOneComponent
+  },
+  {
+    path:'table-two',
+    component:TableTwoComponent
+  },
+  {
+    path:'table-accordion',
+    component:TableAccordionComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableOneComponent } from './table-one/table-one.component';
+import { TableTwoComponent } from './table-two/table-two.component';
+import { TableAccordionComponent } from './table-accordion/table-accordion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableOneComponent,
+    TableTwoComponent,
+    TableAccordionComponent
   ],
   imports: [
     BrowserModule,
