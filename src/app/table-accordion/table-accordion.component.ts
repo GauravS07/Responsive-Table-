c23a467779e99e20e73b0b9e945c4d68f62c4cf1
gaurav.sagar <gaurav.sagar@drcsystems.com>
2021-11-11 02:21:01 +0530

    -accordin

--- src/app/table-accordion/table-accordion.component.ts
+++ src/app/table-accordion/table-accordion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-table-accordion',
@@ -10,6 +11,20 @@ export class TableAccordionComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    // $(function () {
+    //   $(".fold-table tr.view").on("click", function (event) {
+    //     alert("hey");
+    //     $(this).toggleClass("open").next(".fold").toggleClass("open");
+    //   });
+    // });
+  }
+  openAccordin(event: any){
+    $(event.currentTarget).toggleClass("open").next(".fold").toggleClass("open");
+
   }
 
+
+
 }
+
